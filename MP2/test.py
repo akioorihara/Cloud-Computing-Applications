aa6582f9d379debb6b589a5330a0225e36a5e919
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-02-04 08:22:26 -0600

    updating the mp2

--- MP2/test.py
+++ MP2/test.py
@@ -1,17 +1,32 @@
-from flask import Flask, request
-import random 
-
+from flask import Flask, request, jsonify
+import json
+import os 
 
 app = Flask(__name__)
 
-@app.route('/' , methods=['GET', 'POST'])
-def getNum():
-    if request.method == 'POST':
-        return 0
-    random.seed(0)
-    r = str(random.random())
-    return r
+# url = 'https://seorwrpmwh.execute-api.us-east-1.amazonaws.com/prod/mp1'
 
-@app.route('/')
-def postNum():
-    pass
+# payload = {
+# 		'ip_address1':  # <insert ip address:port of first EC2 instance>, 
+# 		'ip_address2':  # <insert ip address:port of secong EC2 instance>,
+# 		'load_balancer' :  # <insert address of load balancer>,
+# 		'submitterEmail':  # <insert your coursera account email>,
+# 		'secret':  # <insert your secret token from coursera>
+# 		}
+
+# r = requests.post(url, data=json.dumps(payload))
+
+# print(r.status_code, r.reason)
+# print(r.text)
+
+@app.route('/json' , methods=['POST'])
+def getNumber():
+    
+    # f = open("application/temp.json", "r")
+    # value = json.load(f)
+    print(request.is_json)
+    req = request.get_json()
+    print(req)
+
+    return "Done"
+
