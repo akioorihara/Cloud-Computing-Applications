bb40d13b883470432feea27455b831d78ea4b14c
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-02-08 22:38:35 -0600

    incomplete parser

--- MP3/main.py
+++ MP3/main.py
@@ -7,6 +7,18 @@ input = '{"graph": "Chicago->Urbana,Urbana->Springfield,Chicago->Lafayette"}'
 graph = re.split(r"['['  \"|\t|,|;|:|{|}|\->]", input)
 while "" in graph:
     graph.remove("")
+source = {}
+destination = {}
+count = 0
+for index in graph:
+    print(index)
+    if count == 0:
+        pass 
+    if count % 1 == 0:
+        source = index 
+    if count % 2 == 0:
+        destination = index
+    count += 1
 
-print(graph)
+print(graph, source, destination)
 
