4f13457c3e85a96a84a99e76cc236dbc957ffe04
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-02-10 08:13:41 -0600

    push update

--- MP3/main.py
+++ MP3/main.py
@@ -4,14 +4,26 @@ import re
 
 input = '{"graph": "Chicago->Urbana,Urbana->Springfield,Chicago->Lafayette"}'
 
+test = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'D'],
+    'D': ['B', 'C', 'E'],
+    'E': ['B', 'D']
+}
 
 def bfs(graph, vertex):
     queue = deque([vertex])
+    # print(queue)
     level = {vertex: 0}
+    # print(level)
     parent = {vertex: None}
+    # print(parent)
     while queue:
         v = queue.popleft()
+        # print(v)
         for n in graph[v]:
+            print(queue.append(n))
             queue.append(n)
             level[n] = level[v] + 1
             parent[n] = v
@@ -38,8 +50,27 @@ def parser(input):
         i += 1
     # graph.pop(0)
     return graph
-print(parser(input))
-print(bfs(parser(input),1))
+# print(parser(input))
+
+
+def buildGraph(string):
+    graph = {}
+    for edge in string.split(','):
+        source, destination = edge.split("->")
+        source = source.strip(" ")
+        destination = destination.strip(" ")
+        if source not in graph:
+            graph[source] = []
+        if destination not in graph:
+            graph[destination] = []
+        graph[source].append(destination)
+    return graph 
+
+print(type(test))
+print(buildGraph(input))
+
+
+# print(bfs(test, 'A'))
 
 # print(graph)
 # print("This is source", source)
