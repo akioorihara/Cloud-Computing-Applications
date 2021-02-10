2ffc1a0e43ce0818d53ca19be33fe1f6d28b9de7
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-02-09 21:38:22 -0600

    parser complete and bfs
    working on breadth first search

--- MP3/main.py
+++ MP3/main.py
@@ -4,25 +4,43 @@ import re
 
 input = '{"graph": "Chicago->Urbana,Urbana->Springfield,Chicago->Lafayette"}'
 
-graph = re.split(r"['['  \"|\t|,|;|:|{|}|\->]", input)
-while "" in graph:
-    graph.remove("")
-source = {}
-destination = {}
-count = 0
-for index in graph:
-    if count == 0:
-        count += 1
-        continue
-    if count % 2 != 0:
-        source[count] = index
-        print(source, "This is count != 0")
-    else: 
-        destination [count]= index
-        print(destination, "This is dest")
-    count += 1
 
-print(graph)
-print("This is source", source)
-print("This is the Dist", destination)
+def bfs(graph, vertex):
+    queue = deque([vertex])
+    level = {vertex: 0}
+    parent = {vertex: None}
+    while queue:
+        v = queue.popleft()
+        for n in graph[v]:
+            queue.append(n)
+            level[n] = level[v] + 1
+            parent[n] = v
+    return level, parent
 
+
+def parser(input):
+    graph = re.split(r"['[' |\"|\|,|;|:|{|}|\->]", input)
+    while "" in graph:
+        graph.remove("")
+    source = []
+    destination = []
+    i = 0
+    for index in graph:
+        if i == 0:
+            i += 1
+            continue
+        if i % 2 != 0:
+            source.append(index)
+            # print(source, "This is count != 2")
+        else:
+            destination.append(index)
+            # print(destination, "This is dest")
+        i += 1
+    # graph.pop(0)
+    return graph
+print(parser(input))
+print(bfs(parser(input),1))
+
+# print(graph)
+# print("This is source", source)
+# print("This is the Dist", destination)
