fa138d4d1df24cf356820426f98dc5b709d4814f
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-02-10 23:09:00 -0600

    Parser and bfs

--- MP3/main.py
+++ MP3/main.py
@@ -4,74 +4,86 @@ import re
 
 input = '{"graph": "Chicago->Urbana,Urbana->Springfield,Chicago->Lafayette"}'
 
-test = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'D'],
-    'D': ['B', 'C', 'E'],
-    'E': ['B', 'D']
-}
-
-def bfs(graph, vertex):
-    queue = deque([vertex])
-    # print(queue)
-    level = {vertex: 0}
-    # print(level)
-    parent = {vertex: None}
-    # print(parent)
-    while queue:
-        v = queue.popleft()
-        # print(v)
-        for n in graph[v]:
-            print(queue.append(n))
-            queue.append(n)
-            level[n] = level[v] + 1
-            parent[n] = v
-    return level, parent
-
-
 def parser(input):
-    graph = re.split(r"['[' |\"|\|,|;|:|{|}|\->]", input)
-    while "" in graph:
-        graph.remove("")
+    graph = {}
+    s = re.split(r"['[' |\"|\|,|;|:|{|}|\->]", input)
+    while "" in s:
+        s.remove("")
+
+    print(s)
     source = []
     destination = []
     i = 0
-    for index in graph:
+    for index in s:
         if i == 0:
             i += 1
             continue
         if i % 2 != 0:
             source.append(index)
-            # print(source, "This is count != 2")
+            # graph[i] = source
+            print(source, "-- This is source val")
         else:
             destination.append(index)
             # print(destination, "This is dest")
         i += 1
-    # graph.pop(0)
+    
+    for edge in s:
+        if source not in s:
+            graph[source]
+        if destination not in s:
+            graph[destination] 
+
     return graph
 # print(parser(input))
 
+# def bfs(graph, root):
+    # print(graph)
+    # visited, queue = set(), deque([root])
+    # visited.add(root)
+    # # print(visited)
+    # while queue:
+    #     vertex = queue.popleft()
+    #     # print(vertex)
+    #     print(str(vertex) + " ", end="")
+    #     # If not visited, mark it as visited, and
+    #     for neighbour in graph[vertex]:
+    #         if neighbour not in visited:
+    #             visited.add(neighbour)
+    #             queue.append(neighbour)
 
-def buildGraph(string):
+
+def bfs(graph, vertex):
+    queue = deque([vertex])
+    level = {vertex: 0}
+    parent = {vertex: None}
+    while queue:
+        v = queue.popleft()
+        for n in graph[v]:
+            if n not in level:            
+                queue.append(n)
+                level[n] = level[v] + 1
+                parent[n] = v
+    return level, parent
+
+def parser(input):
     graph = {}
-    for edge in string.split(','):
-        source, destination = edge.split("->")
-        source = source.strip(" ")
-        destination = destination.strip(" ")
+    for x in input.split(','):
+        source, destination = x.split("->")
         if source not in graph:
             graph[source] = []
         if destination not in graph:
             graph[destination] = []
+        # print(source, "-source ")
+        # print(destination, "-dest")
         graph[source].append(destination)
     return graph 
 
-print(type(test))
-print(buildGraph(input))
 
+# if __name__ == '__main__':
+#     graph = {0: [1, 2], 1: [2], 2: [3], 3: [1, 2]}
+#     print("Following is Breadth First Traversal: ")
+#     bfs(graph, 0)
 
-# print(bfs(test, 'A'))
+print(bfs(parser(input),"Springfield"))      
+# print(parser(input))
 
-# print(graph)
-# print("This is source", source)
-# print("This is the Dist", destination)
