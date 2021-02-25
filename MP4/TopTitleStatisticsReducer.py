080ffb5b6b2b2f0f21c583bceb80a8f1d8f90285
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-02-24 23:41:59 -0600

    update for stats

--- MP4/TopTitleStatisticsReducer.py
+++ MP4/TopTitleStatisticsReducer.py
@@ -6,11 +6,11 @@ import math
 #TODO
 sum = 0
 mean = 0
-min = 0 #I have to fix this later 
+min = 0 
 max = 0 
 var = 0
 count = 0 
-data = {}
+data = []
 
 def check_max(x):
     global max 
@@ -19,19 +19,16 @@ def check_max(x):
 
 def check_min(x):
     global min, count 
-    if count == 1: 
+    if count == 0: 
         min = x
-    if int(min) >= int(x):
+    if int(min) > int(x):
         min = int(x)
-
-def check_ver(x,y):
-    
-    return x 
 
 for line in sys.stdin:
     # TODO
     line = line.strip()
     word, number = line.split('\t')
+    data.append(number)
     check_max(number)
     check_min(number)
     # var = check_ver()
@@ -39,8 +36,16 @@ for line in sys.stdin:
     sum += int(number) 
     count += 1
 
-mean = sum / count
-print(min, max, mean, sum)
+mean = math.floor(sum / count)
+for d in data: 
+    # print(int(mean) - int(d), "This is var")
+    var = var + (int(mean) - int(d))**2
+var = math.floor(var / count)
+
+x = [mean, sum, min, max, var]
+y = ["Mean", "Sum", "Min", "Max", "var"]
+# print(mean, sum, min, max, var)
 
 #TODO
-# print('%s\t%s' % (  ,  )) print as final output
+for index in range(5): 
+    print('%s\t%s' % (y[index],x[index])) #print as final output
