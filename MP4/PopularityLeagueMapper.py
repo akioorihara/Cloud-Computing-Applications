5cdb613c127731eb99b1411bd0be41f137ca94aa
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-03-01 08:31:46 -0600

    updating Part E

--- MP4/PopularityLeagueMapper.py
+++ MP4/PopularityLeagueMapper.py
@@ -25,7 +25,6 @@ for line in sys.stdin:
 for key in mydict:
     if key in league:
        print('%s\t%s' % (key , mydict[key]))
-       # pass
 
        #TODO
        # print('%s\t%s' % (  ,  )) pass this output to reducer
