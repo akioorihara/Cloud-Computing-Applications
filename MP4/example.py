8c1a8f78bdd885e134f78adec1e7682389dba863
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-03-03 23:20:29 -0600

    trying to work on MP5

---
+++ MP4/example.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+
+leaguePath = sys.argv[1]
+#TODO
+league = []
+mydict = {}
+
+with open(leaguePath) as f:
+    #TODO      
+    for line in f: 
+       league.extend([int(x) for x in line.split()]) #int can be read like this 
+    # print(league)
+
+for line in sys.stdin:
+    line.strip()
+    page, count = line.split('\t')
+ 
+    page = int(page)
+    count = int(count)
+    if page not in mydict: 
+       mydict[page] = 0
+    mydict[page] = mydict[page] + count    
+
+
+for key in mydict:
+    if key in league:
+       print('%s\t%s' % (key , mydict[key]))
+
+       #TODO
+       # print('%s\t%s' % (  ,  )) pass this output to reducer
+
+
+
+#compare the only stuff we need to compare and then pass this on to reducer side 
---
+++ MP5/TitleCountSpark.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Exectuion Command: spark-submit TitleCountSpark.py stopwords.txt delimiters.txt dataset/titles/ dataset/output'''
+
+import sys
+from pyspark import SparkConf, SparkContext
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+
+stopWordsPath = sys.argv[1]
+delimitersPath = sys.argv[2]
+inputFile = sys.argv[3]
+
+delimeters = [] 
+stopWords = []
+
+with open(stopWordsPath) as f:
+	#TODO
+    for word in f:
+        stopWords.extend(word.splitlines()) 
+
+with open(delimitersPath) as f:
+    #TODO
+    for line in f: 
+        delimeters.extend(line)
+
+print("hello")
+inputF = [] 
+with open(inputFile, 'r', encoding="utf-8") as f: 
+    for line in f: 
+        inputF.extend(line)
+        print(line.encode('ascii', 'ignore').decode('ascii'))
+
+conf = SparkConf().setMaster("local").setAppName("TitleCount")
+conf.set("spark.driver.bindAddress", "127.0.0.1")
+sc = SparkContext(conf = conf)
+
+# lines = sc.textFile(sys.argv[3],1)
+#TODO
+
+outputFile = open(sys.argv[4],"w")
+
+#TODO
+#write results to output file. Foramt for each line: (line +"\n")
+
+sc.stop()
