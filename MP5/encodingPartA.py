981f69b104489f875c63feb60410f770098905de
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-03-05 20:40:45 -0600

    spark 1 not complete yet

--- MP5/encodingPartA.py
+++ MP5/encodingPartA.py
@@ -16,7 +16,6 @@ with open(stopWordsPath) as f:
 	#TODO
     for word in f:
         stopWords.extend(word.splitlines()) 
-        # allstop = f.read().splitlines()
 
 with open(delimitersPath) as f:
     #TODO
@@ -28,56 +27,64 @@ def splitter(line, delimeters):
         line = line.replace(sep, " ")
     return [i.strip().lower() for i in line.split()]
 
-
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf = conf)
 
 lines = sc.textFile(sys.argv[3],1)
 
-# e = lines.take(20)
-# for i in e:
-#     print(i.encode('ascii', 'ignore').decode('ascii'))
-# def splitter(line):
-#     line = re.sub(r'^\W+|\W+$', '', line)
-#     return map(str.lower, re.split(r'\W+', line))
-
-
-# counts = lines.flatMap(lambda line: line.split()) \
-#              .map(lambda word: (word.lower(), 1)) \
-#              .reduceByKey(lambda a, b: a + b)
-counts = lines.flatMap(lambda line: splitter(line, delimeters).strip().split()) \
-             .map(lambda word: (word.lower(), 1)) \
+counts = lines.flatMap(lambda line: line.split(" ")) \
+             .map(lambda word: (word, 1)) \
              .reduceByKey(lambda a, b: a + b)
 
+### From here
+punc = []
+def lower_clean(x):
+    punc = delimeters
+    lowered = x.lower()
+    for i in punc:
+        lowered = lowered.replace(i, '')
+    return lowered
 
-output = lines.collect()
+lines = lines.map(lower_clean)
+lines.take(100)
+lines = lines.flatMap(lambda x: x.split(" "))
+lines.take(10)
+lines = lines.filter(lambda x: x !='')
+lines.take(4)
+word_count = lines.map(lambda word: (word, 1))
+word_count.take(10)
+word_count = word_count.reduceByKey(lambda x, y: (x+y)).sortByKey()
+word_count.take(50)
+word_count = word_count.map(lambda x : (x[1], x[0]))
+word_count.take(30)
+
+###
+
+
+output = counts.collect()
+# print(output)
 
 total = {}
-for line in output:
+for (line, count) in output:
     line.strip()
     token = splitter(line, delimeters)
-    # print('%s\t%s' % (token[0].encode('utf-8'), count))
     token = [word for word in token if word not in stopWords]
     token = [word for word in token if word not in '']
     
-    for each in token:
-        if each in total:
-            total[each] += 1
-        else:   
-            total[each] = 1
-        # print("%s: %i" % (each.encode('ascii', 'ignore').decode('ascii'), count))
+    for each in token: 
+        total[each] = int(count)
+        # print("%s: %i" % (each.encode('utf-8'), count))
 
 
-# output.saveAsTextFile(sys.argv[4])
-outputFile = open(sys.argv[4],"w")  #probably this way to save in a one file 
+# counts.saveAsTextFile(sys.argv[4])
+outputFile = open(sys.argv[4],"w")
 swords = sorted(total.items(), key = lambda x: (x[1], x[0]), reverse=True)
-
 x = swords[:10] 
 for word in x[::-1]:
     # print('%s\t%s' % (word[0].encode('utf-8'), word[1]))
     outputFile.write(str(word[0].encode('utf-8')) + '\t' + str(word[1]) + '\n')
-    # pass
+    
 
 
 
