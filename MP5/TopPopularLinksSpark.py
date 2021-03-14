24580b059ea0785a884a808bca0f0ddd9b094e11
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-03-14 06:31:29 -0500

    part D

--- MP5/TopPopularLinksSpark.py
+++ MP5/TopPopularLinksSpark.py
@@ -8,49 +8,28 @@ sc = SparkContext(conf = conf)
 
 lines = sc.textFile(sys.argv[1], 1) 
 
-#TODO
+#TODO 
+split_lines = lines.map(lambda x: x.split(":"))
+fromPages = split_lines.map(lambda x: (x[0], 0))
+toPages = split_lines.flatMap(lambda x: x[1].strip().split())
+toPages = toPages.map(lambda x: (x, 1))
+allPages =  fromPages.union(toPages)  
+allPages = allPages.reduceByKey(lambda x, y: x+y)
+# text = allPages.filter(lambda x: x[1] == 0).collect()
+PopularPages = allPages.collect()
 
-output = open(sys.argv[2], "w")
 
-#TODO
-#write results to output file. Foramt for each line: (key + \t + value +"\n")
+PopularPages = sorted(PopularPages, key= lambda x: x[1], reverse=True)
+PopularPages = PopularPages[:10] 
+PopularPages = sorted(PopularPages)
+# PopularPages = sorted(PopularPages.items(), key = lambda x: x[1], reverse=True)
 
-sc.stop()
-#!/usr/bin/env python
-import sys, math
-from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster("local").setAppName("TopTitleStatistics")
-conf.set("spark.driver.bindAddress", "127.0.0.1")
-sc = SparkContext(conf = conf)
 
-lines = sc.textFile(sys.argv[1],1).map(lambda line:( int(line.strip().split('\t')[1])))
-# rdd = sc.parallelize(lines)
-# sum = rdd.map(lambda x: x + x)
-# print(sum)
-
-#TODO  
-sum = sc.accumulator(0)
-def mysum(x):
-    sum.add(x)
-# min = sc.accumulator(1000000)
-min = lines.min()
-max = lines.max()
-mean = int(lines.mean())
-var = math.ceil(lines.variance())
-# print("Min : ", min, "Max : ", max, "Mean :", mean, "Var :", var)
-
-lines.foreach(mysum)
-
-outputFile = open(sys.argv[2],"w")
-
-#TODO write your output here'''
-#write results to output file. Format
-outputFile.write('Mean\t%s\n' % mean)
-outputFile.write('Sum\t%s\n' % sum)
-outputFile.write('Min\t%s\n' % min)
-outputFile.write('Max\t%s\n' % max)
-outputFile.write('Var\t%s\n' % var)
+output = open(sys.argv[2], "w") #Write output to files 
+for link in PopularPages:
+  #  print('%s\t%s' % (link[0], link[1]))
+  output.write(str(link[0]) + '\t' + str(link[1]) + '\n')
 
 sc.stop()
 
--- MP5/TopTitleStatisticsSpark.py
+++ MP5/TopTitleStatisticsSpark.py
@@ -2,24 +2,54 @@
 import sys
 from pyspark import SparkConf, SparkContext
 
+conf = SparkConf().setMaster("local").setAppName("TopPopularLinks")
+conf.set("spark.driver.bindAddress", "127.0.0.1")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile(sys.argv[1], 1) 
+
+#TODO
+
+output = open(sys.argv[2], "w")
+
+#TODO
+#write results to output file. Foramt for each line: (key + \t + value +"\n")
+
+sc.stop()
+#!/usr/bin/env python
+import sys, math
+from pyspark import SparkConf, SparkContext
+
 conf = SparkConf().setMaster("local").setAppName("TopTitleStatistics")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile(sys.argv[1],1)
+lines = sc.textFile(sys.argv[1],1).map(lambda line:( int(line.strip().split('\t')[1])))
+# rdd = sc.parallelize(lines)
+# sum = rdd.map(lambda x: x + x)
+# print(sum)
 
-#TODO
+#TODO  
+sum = sc.accumulator(0)
+def mysum(x):
+    sum.add(x)
+# min = sc.accumulator(1000000)
+min = lines.min()
+max = lines.max()
+mean = int(lines.mean())
+var = math.ceil(lines.variance())
+# print("Min : ", min, "Max : ", max, "Mean :", mean, "Var :", var)
+
+lines.foreach(mysum)
 
 outputFile = open(sys.argv[2],"w")
-'''
-TODO write your output here
-write results to output file. Format
-outputFile.write('Mean\t%s\n' % ans1)
-outputFile.write('Sum\t%s\n' % ans2)
-outputFile.write('Min\t%s\n' % ans3)
-outputFile.write('Max\t%s\n' % ans4)
-outputFile.write('Var\t%s\n' % ans5)
-'''
+
+#TODO write your output here'''
+#write results to output file. Format
+outputFile.write('Mean\t%s\n' % mean)
+outputFile.write('Sum\t%s\n' % sum)
+outputFile.write('Min\t%s\n' % min)
+outputFile.write('Max\t%s\n' % max)
+outputFile.write('Var\t%s\n' % var)
 
 sc.stop()
-
