641f36fc447f2a53a6d99b12b95101045aa08f7a
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-03-30 13:20:16 -0500

    completed on this stamp

--- MP7/TablePartE.py
+++ MP7/TablePartE.py
@@ -3,6 +3,10 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-for key, data in ???:
+connection = hb.Connection()
+connection.open()
+t = connection.table('powers')
+for key, data in t.scan( include_timestamp=True):
     print('Found: {}, {}'.format(key, data))
 
+connection.close()
