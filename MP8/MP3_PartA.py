90d878cf9ce93cf476af3a4808ec5ea3e71edc64
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-04-03 23:17:28 -0500

    passed the part A

--- MP8/MP3_PartA.py
+++ MP8/MP3_PartA.py
@@ -1,7 +1,9 @@
 from pyspark import SparkContext, SQLContext
+from pyspark import Row
 from pyspark.sql.types import StructType
 from pyspark.sql.types import StructField
 from pyspark.sql.types import StringType, IntegerType
+from pyspark.sql.types import *
 
 sc = SparkContext()
 sqlContext = SQLContext(sc)
@@ -10,12 +12,14 @@ sqlContext = SQLContext(sc)
 # 1. Setup (10 points): Download the gbook file and write a function to load it in an RDD & DataFrame
 ####
 
-# RDD API
-# Columns:
-# 0: place (string), 1: count1 (int), 2: count2 (int), 3: count3 (int)
-
-
-# Spark SQL - DataFrame API
-
-
-
+textFile = sc.textFile('gbooks')
+split_lines = textFile.map(lambda x: x.split('\t'))
+# line = split_lines.map(lambda x: Row(word=x[0], count1=int(x[1]),count2=int(x[2]), count3=int(x[3])))
+schema = StructType([
+    StructField('word', StringType(), True),
+    StructField('count1', IntegerType(), True),
+    StructField('count2', IntegerType(), True),
+    StructField('count3', IntegerType(), True)
+    ])
+df = sqlContext.createDataFrame(split_lines, schema) 
+df.printSchema()
